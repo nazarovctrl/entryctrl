87414a5ca73dcc31eea7f4e89c30e7934bc6dc2f
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-04 15:59:33 +0500

    Telegram bot ui started

---
+++ src/main/java/uz/nazarovctrl/entryctrl/exp/TelegramBotException.java
@@ -0,0 +1,8 @@
+package uz.nazarovctrl.entryctrl.exp;
+
+
+public class TelegramBotException extends Exception {
+    public TelegramBotException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/uz/nazarovctrl/entryctrl/telegrambot/MessageService.java
@@ -0,0 +1,32 @@
+package uz.nazarovctrl.entryctrl.telegrambot;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import io.github.nazarovctrl.telegrambotspring.bot.MessageSender;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Service
+public class MessageService {
+    private final MessageSender messageSender;
+
+    public MessageService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public Message sendMessage(SendMessage sendMessage) {
+        try {
+            return messageSender.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Message sendMessage(String chatId, String message) {
+        return sendMessage(new SendMessage(chatId, message));
+    }
+
+    public Message sendMessage(Long chatId, String message) {
+        return sendMessage(chatId.toString(), message);
+    }
+}
