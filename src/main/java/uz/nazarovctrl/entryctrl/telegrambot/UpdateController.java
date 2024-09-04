ccb9c09d9f0c782a83e6395c687fc9e255c49db1
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-04 15:59:43 +0500

    Telegram bot ui started

--- src/main/java/uz/nazarovctrl/entryctrl/telegrambot/UpdateController.java
+++ src/main/java/uz/nazarovctrl/entryctrl/telegrambot/UpdateController.java
@@ -1,15 +1,30 @@
 package uz.nazarovctrl.entryctrl.telegrambot;
 
-import io.github.nazarovctrl.telegrambotspring.controller.AbstractUpdateController;
+import uz.nazarovctrl.entryctrl.exp.TelegramBotException;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import io.github.nazarovctrl.telegrambotspring.context.UserContext;
+import io.github.nazarovctrl.telegrambotspring.controller.AbstractUpdateController;
 
 @Component
 public class UpdateController extends AbstractUpdateController {
+    private final MessageController messageController;
+    private final MessageService messageService;
+
+    public UpdateController(MessageController messageController, MessageService messageService) {
+        this.messageController = messageController;
+        this.messageService = messageService;
+    }
 
     @Override
     public void handle(Update update) {
-
+        try {
+            if (update.hasMessage()) {
+                messageController.handle(update.getMessage());
+            }
+        } catch (TelegramBotException e) {
+            messageService.sendMessage(UserContext.getUser().getId(), e.getMessage());
+        }
     }
 }
